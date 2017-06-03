3ec72b3af9adc341d3dcc3284dd7f7b4f22b793e
Phil <Phil@Cathys-MacBook-Pro.local>
2017-06-03 17:48:14 -0400

    minor updates

--- assets/javascript/meal-plan.js
+++ assets/javascript/meal-plan.js
@@ -1,17 +1,24 @@
 var AppID = "4415471a";
 var APIKey = "ab3e2bd0953f0ffa8814cf394bba9d8f";
 
+var planSearches = [];
+
 $(document).ready(function () {
-    $('select').material_select();
+	$('select').material_select();
+
+	// sets sticky footer
+	$(".page-footer").css("position", "absolute");
+	$(".page-footer").css("bottom", "0");
+	$(".page-footer").css("left", "0");
 });
 
-function executeSearch (dietSearch, allergySearch) {
+function executeSearch(dietSearch, allergySearch) {
 	var queryURL = `http://api.yummly.com/v1/api/recipes?_app_id=${ AppID }&_app_key=${ APIKey }&q=${ encodeURI(dietSearch) }&allowedAllergy[]=${ encodeURI(allergySearch)}`;
 	$.ajax({
 		url: queryURL
-	}).done(function(res){
+	}).done(function (res) {
 		// console.log(res)
-	
+
 		// clearing the result boxes
 		$('#breakfast-results').empty();
 		$('#lunch-results').empty();
@@ -22,12 +29,12 @@ function executeSearch (dietSearch, allergySearch) {
 		var breakfastResName = breakfastRes.recipeName;
 		var breakfastResIngredient = breakfastRes.ingredients;
 		var breakfastResImage = breakfastRes.imageUrlsBySize[90];
-	
+
 		var breakfastDiv = $('<div class="breakfast-item">');
 		var breakfastName = $(`<p class="breakfast-item_name">${ breakfastResName }</p>`);
 		var breakfastImage = $('<img src="' + `${breakfastResImage}` + '"/>')
 		var breakfastIngredients = $(`<p class="breakfast-item_ingredients">${ breakfastResIngredient}</p>`)
-		
+
 		breakfastDiv.append(breakfastName);
 		breakfastDiv.append(breakfastImage);
 		breakfastDiv.append(breakfastIngredients);
@@ -38,12 +45,12 @@ function executeSearch (dietSearch, allergySearch) {
 		var lunchResName = lunchRes.recipeName;
 		var lunchResIngredient = lunchRes.ingredients;
 		var lunchResImage = lunchRes.imageUrlsBySize[90];
-	
+
 		var lunchDiv = $('<div class="lunch-item">');
 		var lunchName = $(`<p class="lunch-item_name">${ lunchResName }</p>`);
 		var lunchImage = $('<img src="' + `${lunchResImage}` + '"/>')
 		var lunchIngredients = $(`<p class="lunch-item_ingredients">${ lunchResIngredient}</p>`)
-		
+
 		lunchDiv.append(lunchName);
 		lunchDiv.append(lunchImage);
 		lunchDiv.append(lunchIngredients);
@@ -54,12 +61,12 @@ function executeSearch (dietSearch, allergySearch) {
 		var dinnerResName = dinnerRes.recipeName;
 		var dinnerResIngredient = dinnerRes.ingredients;
 		var dinnerResImage = dinnerRes.imageUrlsBySize[90];
-	
+
 		var dinnerDiv = $('<div class="dinner-item">');
 		var dinnerName = $(`<p class="dinner-item_name">${ dinnerResName }</p>`);
 		var dinnerImage = $('<img src="' + `${dinnerResImage}` + '"/>')
 		var dinnerIngredients = $(`<p class="dinner-item_ingredients">${ dinnerResIngredient}</p>`)
-		
+
 		dinnerDiv.append(dinnerName);
 		dinnerDiv.append(dinnerImage);
 		dinnerDiv.append(dinnerIngredients);
@@ -67,9 +74,30 @@ function executeSearch (dietSearch, allergySearch) {
 	});
 }
 
-$('#plan-search').on('click', function(search){
+function renderMeals() {
+	for (var i = 0; i < planSearches.length; i++) {
+		var list = $("<li>");
+		var newSearch = $("<a href='#'>");
+		newSearch.addClass("searches");
+		newSearch.text(planSearches[i]);
+		newSearch.attr("value", planSearches[i]);
+
+		list.append(newSearch);
+
+		$("#recent-search").append(list);
+	}
+}
+
+$('#plan-search').on('click', function (search) {
 	search.preventDefault();
+
+	$(".page-footer").css("position", "inherit");
+
 	var dietChoice = $("#diet-type").val();
 	var allergyChoice = $("#allery-type").val();
 	executeSearch(dietChoice, allergyChoice);
+
+	planSearches.push($("#diet-type"))
+
+	renderMeals();
 });
