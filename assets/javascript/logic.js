2ef81ede5f25a37857234c3c236bd42eba9e8834
Villejoint <kvillejo@estee.com>
2017-06-04 16:51:55 -0400

    Edit html/css, add google login button

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -23,3 +23,22 @@ $("#meal-results").hide();
 $("#plan-search").on("click", function() {
     $("#meal-results").show();
 });
+
+function onSuccess(googleUser) {
+    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+    }
+
+function onFailure(error) {
+	console.log(error);
+}
+function renderButton() {
+    gapi.signin2.render('my-signin2', {
+        'scope': 'profile email',
+        'width': 240,
+        'height': 50,
+        'longtitle': true,
+        'theme': 'dark',
+        'onsuccess': onSuccess,
+        'onfailure': onFailure
+    });
+}
