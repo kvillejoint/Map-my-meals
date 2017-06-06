578601373c323b16d80476fcdd913ace69e62dd4
mdnurul <nurul.alam1@hotmail.com>
2017-06-06 19:08:41 -0400

    adds logic

--- assets/javascript/google-api.js
+++ assets/javascript/google-api.js
@@ -10,3 +10,19 @@
     messagingSenderId: "276525470032"
   };
   firebase.initializeApp(config);
+
+  //Get email and password
+
+  var email = $("#mail").val().trim();
+  var pass = $("#password").val().trim();
+  var auth = firebase.auth();
+
+  //Sign in email and password
+  $("#login").on("click", function() {
+      firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorMessage);
+    });
+  });
