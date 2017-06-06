d38eaa22b68924475632ccaf50d66fabe6d22efa
mdnurul <nurul.alam1@hotmail.com>
2017-06-06 18:41:23 -0400

    login js add

---
+++ assets/javascript/google-api.js
@@ -0,0 +1,12 @@
+
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCFDjMEyTQodkgBbVbbZ9DMfzDobs0n6lU",
+    authDomain: "project-1-50c42.firebaseapp.com",
+    databaseURL: "https://project-1-50c42.firebaseio.com",
+    projectId: "project-1-50c42",
+    storageBucket: "project-1-50c42.appspot.com",
+    messagingSenderId: "276525470032"
+  };
+  firebase.initializeApp(config);
